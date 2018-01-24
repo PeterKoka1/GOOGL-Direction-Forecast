9ca87747c18ea584336721b82d1a1473db3dff6e
Peter Kokalov <petervkokalov@gmail.com>
2018-01-24 03:56:57 -0500

    Update Backtest.py

--- Backtest.py
+++ Backtest.py
@@ -1,25 +1,38 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
 
 def back_test():
     df = pd.read_csv('final.csv')
+
     df.drop(labels=['Unnamed: 0'], axis=1, inplace=True)
-    df.rename(columns={'col1': 'trade_sig', 'col2': 'close', 'Delt.1.arithmetic': 'daily_rets'}, inplace=True)
-    df['ports_rets'] = df['daily_rets'] * df['trade_sig'].shift(1)
-    df['cum_rets'] = df['ports_rets'].cumsum()
-    plt.plot(df['cum_rets'])
-    plt.title("$GOOGL SVM Portfolio")
-    plt.show()
-    print(df.head(20))
-    print("\nBacktest $GOOGL Buy-Only SVM Portfolio:\n")
-    print("Backtest period from 2015-05-27 to 2018-01-18")
-    print("Returns: {}".format(
-        str(df['cum_rets'][667]*100)[0:5]) + "%"
+    df.rename(columns={'col1': 'trade_sig_svm', 'col2': 'trade_sig_lr', 'col3': 'trade_sig_qda',
+                       'col4': 'close', 'Delt.1.arithmetic': 'daily_rets'}, inplace=True)
+    df['ports_rets_svm'] = df['daily_rets'] * df['trade_sig_svm'].shift(1)
+    df['cum_rets_svm'] = df['ports_rets_svm'].cumsum()
+    df['ports_rets_lr'] = df['daily_rets'] * df['trade_sig_lr'].shift(1)
+    df['cum_rets_lr'] = df['ports_rets_lr'].cumsum()
+    df['ports_rets_qda'] = df['daily_rets'] * df['trade_sig_qda'].shift(1)
+    df['cum_rets_qda'] = df['ports_rets_qda'].cumsum()
+    print("\nBacktest $GOOGL Long-Short SVM Portfolio:\n")
+    print("Backtest period from 2015-07-08 to 2018-01-16")
+    print("SVM Returns: {}".format(
+        str(df['cum_rets_svm'][636]*100)[0:5]) + "%"
     )
+    print("Log Reg Returns: {}".format(
+        str(df['cum_rets_lr'][636] * 100)[0:5]) + "%"
+          )
+    print("QDA Returns: {}".format(
+        str(df['cum_rets_qda'][636] * 100)[0:5]) + "%"
+          )
     try:
-        pd.DataFrame(df['cum_rets']).join(df['daily_rets']).to_csv('Returns.csv')
+        final_preds = pd.DataFrame(df['daily_rets'])
+        final_preds['SVM'] = df['cum_rets_svm']
+        final_preds['QDA'] = df['cum_rets_qda']
+        final_preds['LogReg'] = df['cum_rets_lr']
+        final_preds.to_csv('Returns.csv')
         print("'Returns.csv' successfully saved!")
     except:
         print("Unable to save csv.")
+
+back_test()
